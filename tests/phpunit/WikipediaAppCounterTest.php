7c78f6a6e44e5f90e58fa1a433809a9ce531d87d
Michael Holloway <mholloway@wikimedia.org>
2020-03-10 16:31:15 -0400

    Fix revert counting for non-language-specific counters
    The change in d0fd394 did not update revert counting according to the
    new treatment for non-language-specific counters; hence, reverts for
    those counters (including the new Depicts counter) will not be counted
    correctly without this change.
    
    We also need to check that the action in the edit summary matches the
    specified action for the counter, or else reverts will be counted once by
    each language-agnostic counter.
    
    This patch also adds some new unit tests around edit and revert counting
    to increase confidence in the counters and help prevent regressions.
    Change tag addition and checking is moved out of conditionallyIncrement
    methods to facilitate testing those methods. Also removed is the check
    that MW_API is defined; it is redundant with the 'action' parameter
    check and complicates testing.
    
    Bug: T244974
    Change-Id: Ibd80bfe839b732e577abea5c7f55dbda5da49f42

--- tests/phpunit/WikipediaAppCounterTest.php
+++ tests/phpunit/WikipediaAppCounterTest.php
@@ -2,41 +2,207 @@
 
 namespace MediaWiki\Extension\WikimediaEditorTasks\Test;
 
+use CommentStoreComment;
+use FauxRequest;
+use MediaWiki\Extension\WikimediaEditorTasks\CounterDao;
+use MediaWiki\Extension\WikimediaEditorTasks\Utils;
 use MediaWiki\Extension\WikimediaEditorTasks\WikimediaEditorTasksServices;
+use MediaWiki\Extension\WikimediaEditorTasks\WikipediaAppCounter;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionSlots;
+use MediaWiki\Revision\RevisionStore;
+use MediaWiki\Revision\RevisionStoreRecord;
+use MediaWiki\Revision\SlotRecord;
 use MediaWikiTestCase;
+use TextContent;
+use Title;
+use User;
+use WebRequest;
 use Wikimedia\TestingAccessWrapper;
 
-/** @covers \MediaWiki\Extension\WikimediaEditorTasks\WikipediaAppCounter */
+/**
+ * @group Database
+ * @covers \MediaWiki\Extension\WikimediaEditorTasks\WikipediaAppCounter
+ */
 class WikipediaAppCounterTest extends MediaWikiTestCase {
 
-	/** @var TestCounter */
-	private $counter;
+	const DESCRIPTION_COMMENT = '/* wbsetdescription-add:1|zh */ 韓國高速鐵道, #suggestededit-add 1.0';
+	const CAPTION_COMMENT = '/* wbsetlabel-add:1|zh-hant */ 韓國高速鐵道, #suggestededit-add 1.0';
+	const DEPICTS_COMMENT = '/* wbsetclaim-create:2||1 */ [[d:Special:EntityPage/P180]]: ' .
+		'[[d:Special:EntityPage/Q42]], #suggestededit-add 1.0';
+
+	/** @var User */
+	private $user;
+
+	/** @var RevisionStore */
+	private $revisionStore;
+
+	/** @var WikipediaAppCounter[] */
+	private $counters;
+
+	/** @var CounterDao */
+	private $counterDao;
 
 	public function setUp(): void {
 		parent::setUp();
-		$counterFactory = WikimediaEditorTasksServices::getInstance()->getCounterFactory();
-		$this->counter = TestingAccessWrapper::newFromObject( $counterFactory->create( [
-			"class" => "MediaWiki\\Extension\\WikimediaEditorTasks\\Test\\TestCounter",
-			"counter_key" => "test"
+		global $wgWikimediaEditorTasksEnableEditStreaks, $wgWikimediaEditorTasksEnableRevertCounts;
+		$wgWikimediaEditorTasksEnableEditStreaks = true;
+		$wgWikimediaEditorTasksEnableRevertCounts = true;
+
+		$this->tablesUsed = array_merge( $this->tablesUsed, [
+			'wikimedia_editor_tasks_keys',
+			'wikimedia_editor_tasks_counts',
+			'wikimedia_editor_tasks_edit_streak'
+		] );
+
+		$this->revisionStore = MediaWikiServices::getInstance()->getRevisionStore();
+
+		$extensionServices = WikimediaEditorTasksServices::getInstance();
+		$this->counterDao = $extensionServices->getCounterDao();
+
+		$counterFactory = $extensionServices->getCounterFactory();
+		$this->counters = array_map( function ( $counter ) {
+			return TestingAccessWrapper::newFromObject( $counter );
+		}, $counterFactory->createAll( [
+			[
+				"class" => "MediaWiki\\Extension\\WikimediaEditorTasks\\WikipediaAppDescriptionEditCounter",
+				"counter_key" => "app_description_edits"
+			],
+			[
+				"class" => "MediaWiki\\Extension\\WikimediaEditorTasks\\WikipediaAppCaptionEditCounter",
+				"counter_key" => "app_caption_edits"
+			],
+			[
+				"class" => "MediaWiki\\Extension\\WikimediaEditorTasks\\WikipediaAppImageDepictsEditCounter",
+				"counter_key" => "app_depicts_edits"
+			],
 		] ) );
+
+		$this->user = $this::getTestUser()->getUser();
+	}
+
+	public function testIncrementDescriptionEditCount() {
+		$id = Utils::getCentralId( $this->user );
+		$request = $this->getRequest( 'wbsetdescription' );
+		$revision = $this->getRevision( self::DESCRIPTION_COMMENT );
+		foreach ( $this->counters as $counter ) {
+			$counter->conditionallyIncrementEditCount( $id, $request, $revision );
+		}
+		$this->assertEquals( 1, $this->getEditCountForCounter( $id, 'app_description_edits' ) );
+		foreach ( $this->counters as $counter ) {
+			$counter->conditionallyIncrementRevertCount( $id, $revision );
+		}
+		$this->assertEquals( 1, $this->getRevertCountForCounter( $id, 'app_description_edits' ) );
+	}
+
+	public function testIncrementCaptionEditCount() {
+		$id = Utils::getCentralId( $this->user );
+		$request = $this->getRequest( 'wbsetlabel' );
+		$revision = $this->getRevision( self::CAPTION_COMMENT );
+		foreach ( $this->counters as $counter ) {
+			$counter->conditionallyIncrementEditCount( $id, $request, $revision );
+		}
+		$this->assertEquals( 1, $this->getEditCountForCounter( $id, 'app_caption_edits' ) );
+		foreach ( $this->counters as $counter ) {
+			$counter->conditionallyIncrementRevertCount( $id, $revision );
+		}
+		$this->assertEquals( 1, $this->getRevertCountForCounter( $id, 'app_caption_edits' ) );
+	}
+
+	public function testIncrementDepictsEditCount() {
+		$id = Utils::getCentralId( $this->user );
+		$request = $this->getRequest( 'wbsetclaim' );
+		$revision = $this->getRevision( self::DEPICTS_COMMENT );
+		foreach ( $this->counters as $counter ) {
+			$counter->conditionallyIncrementEditCount( $id, $request, $revision );
+		}
+		$this->assertEquals( 1, $this->getEditCountForCounter( $id, 'app_depicts_edits' ) );
+		foreach ( $this->counters as $counter ) {
+			$counter->conditionallyIncrementRevertCount( $id, $revision );
+		}
+		$this->assertEquals( 1, $this->getRevertCountForCounter( $id, 'app_depicts_edits' ) );
+	}
+
+	public function testIsWikipediaAppRequest() {
+		$this->assertTrue( $this->counters[0]->isWikipediaAppRequest( $this->getRequest( 'query' ) ) );
+		$this->assertFalse( $this->counters[0]->isWikipediaAppRequest( new FauxRequest() ) );
 	}
 
 	public function testGetLanguageFromQualifyingComment() {
-		$comment = '/* wmettestedit-add:1|zh-hant */ 韓國高速鐵道, #suggestededit-add 1.0';
-		$lang = $this->counter->getLanguageFromWikibaseComment( $comment );
-		$this->assertSame( 'zh-hant', $lang );
+		$lang = $this->counters[0]->getLanguageFromWikibaseComment( self::DESCRIPTION_COMMENT );
+		$this->assertSame( 'zh', $lang );
 	}
 
 	public function testGetLanguageFromNonQualifyingWikibaseComment() {
-		$comment = '/* wmettestedit-add:1|zh-hant */ 韓國高速鐵道';
-		$lang = $this->counter->getLanguageFromWikibaseComment( $comment );
+		$comment = '/* wbsetdescription-add:1|zh-hant */ 韓國高速鐵道';
+		$lang = $this->counters[0]->getLanguageFromWikibaseComment( $comment );
 		$this->assertNull( $lang );
 	}
 
 	public function testGetLanguageFromNonQualifyingNonWikibaseComment() {
 		$comment = 'Foo';
-		$lang = $this->counter->getLanguageFromWikibaseComment( $comment );
+		$lang = $this->counters[0]->getLanguageFromWikibaseComment( $comment );
 		$this->assertNull( $lang );
+	}
+
+	/**
+	 * @param int $id central user id
+	 * @param string $key counter key
+	 * @return int
+	 */
+	private function getEditCountForCounter( int $id, string $key ): int {
+		$countsByCounter = $this->counterDao->getAllEditCounts( $id );
+		$countsByLang = $countsByCounter[$key];
+		return array_sum( array_values( $countsByLang ) );
+	}
+
+	/**
+	 * @param int $id central user id
+	 * @param string $key counter key
+	 * @return int
+	 */
+	private function getRevertCountForCounter( int $id, string $key ): int {
+		$countsByCounter = $this->counterDao->getAllRevertCounts( $id );
+		$countsByLang = $countsByCounter[$key];
+		return array_sum( array_values( $countsByLang ) );
+	}
+
+	/**
+	 * @param string $action
+	 * @return WebRequest
+	 */
+	private function getRequest( string $action ): WebRequest {
+		$request = new FauxRequest( [ 'action' => $action ] );
+		$request->setHeader( 'User-agent', 'WikipediaApp/WikipediaAppCounterTest' );
+		return $request;
+	}
+
+	/**
+	 * @param string comment
+	 * @return RevisionStoreRecord
+	 */
+	private function getRevision( string $comment = '' ): RevisionStoreRecord {
+		$title = Title::newFromText( 'Dummy' );
+		$title->resetArticleID( 177 );
+
+		$comment = CommentStoreComment::newUnsavedComment( $comment );
+
+		$main = SlotRecord::newUnsaved( SlotRecord::MAIN, new TextContent( 'Lorem Ipsum' ) );
+		$slots = new RevisionSlots( [ $main ] );
+
+		$row = [
+			'rev_id' => '7',
+			'rev_page' => strval( $title->getArticleID() ),
+			'rev_timestamp' => '20200101000000',
+			'rev_deleted' => 0,
+			'rev_minor_edit' => 0,
+			'rev_parent_id' => '5',
+			'rev_len' => $slots->computeSize(),
+			'rev_sha1' => $slots->computeSha1(),
+			'page_latest' => '178',
+		];
+
+		return new RevisionStoreRecord( $title, $this->user, $comment, (object)$row, $slots );
 	}
 
 }
